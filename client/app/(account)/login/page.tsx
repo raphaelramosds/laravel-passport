0b370039d17c49f5bbad62538d975e932ac72f64
Raphael Ramos <raphaelramos@geowellex.com>
2025-04-15 22:02:32 -0300

    feat: log in and see profile data

--- client/app/(account)/login/page.tsx
+++ client/app/(account)/login/page.tsx
@@ -1,19 +1,73 @@
+'use client'
+
+import axios from "axios";
+
 import Link from "next/link";
 
+import { useState } from "react";
+import { redirect } from "next/navigation";
+
 export default function Login() {
-    return ( 
+    
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [credentials, setCredentials] = useState<{
+        email: string;
+        password: string;
+    }>({
+        email: '',
+        password: ''
+    });
+
+    const handleSubmit = function (event) {
+        event.preventDefault();
+
+        const data = {
+            email: credentials.email,
+            password: credentials.password
+        };
+
+        axios.post('http://localhost:89/api/login', data, {
+            headers: {
+                "Content-Type" : "application/json",
+            }
+        })
+            .then((response) => {
+                localStorage.setItem('token', response.data.token);
+                setLoggedIn(true);
+                alert("Login successful");
+            })
+            .catch((error) => {
+                alert(error.response.data.message);
+            });
+    }
+
+    if (loggedIn) {
+        redirect('/profile');
+    }
+
+    return (
         <div className="container mt-5">
             <div className="jumbotron col-lg-4 offset-lg-4">
                 <p className="lead">Please login to your account.</p>
                 <hr className="my-4" />
-                <form method="POST">
+                <form method="POST" onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" className="form-control" id="email" name="email" required />
+                        <input type="email" className="form-control" id="email" name="email" onChange={(event) => {
+                            setCredentials({
+                                ...credentials,
+                                email: event.target.value
+                            });
+                        }} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" id="password" name="password" required />
+                        <input type="password" className="form-control" id="password" name="password" onChange={(event) => {
+                            setCredentials({
+                                ...credentials,
+                                password: event.target.value
+                            });
+                        }} />
                     </div>
                     <button type="submit" className="btn btn-primary">Login</button>
                     <div className="mt-3 text-center">
--- client/app/(account)/profile/page.tsx
+++ client/app/(account)/profile/page.tsx
@@ -1,6 +1,43 @@
-import Image from "next/image";
+'use client';
+
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Profile() {
+
+    const [user, setUser] = useState<{
+        email: string;
+        name: string;
+    }>({
+        email: '',
+        name: ''
+    });
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        if (!token) {
+            console.warn("No token found");
+            return;
+        }
+
+        axios.get("http://localhost:89/api/user", {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then((response) => {
+                setUser({
+                    email: response.data.email,
+                    name: response.data.name
+                });
+            })
+            .catch((error) => {
+                console.error("There was an error fetching the user data!", error);
+            });
+    }, []);
+
     return (
         <div className="container mt-5">
             <div className="card mr-auto ml-auto" style={{ width: "18rem" }}>
@@ -8,9 +45,9 @@ export default function Profile() {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
                 <div className="card-body">
-                    <h5 className="card-title">Hi, user!</h5>
+                    <h5 className="card-title">Hi, {user.name}!</h5>
                     <ul className="list-group">
-                        <li className="list-group-item">Email: email@example.com</li>
+                        <li className="list-group-item">Email: {user.email}</li>
                     </ul>
                 </div>
             </div>
